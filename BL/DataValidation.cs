5b779199b7fcd3588a9f9dbbb6e545a2a937f43f
SharpeningInCSharp <65521226+SharpeningInCSharp@users.noreply.github.com>
2021-05-22 15:14:30 +1000

    Date and name validation rules

--- BL/DataValidation.cs
+++ BL/DataValidation.cs
@@ -1,13 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Windows.Controls;
 
 namespace BL
 {
-	public static class DataValidation
+	public static partial class DataValidation
 	{
-		//SOLVE: returns ValidationResult
-		//TODO: date validation
 		//TODO: phone validation
+
+		public static ValidationResult ValidateName(string name)
+		{
+			if (IsNameValid(name))
+				return ValidationResult.ValidResult;
+
+			return new ValidationResult(false, InvalidNameMessage);
+		}
+
+		public static ValidationResult ValidateDate(string date)
+		{
+			var isParsed = DateTime.TryParseExact(date, "G",
+											CultureInfo.CurrentCulture,
+											DateTimeStyles.None,
+											out var dateTime);
+
+			if (isParsed == false)
+				return new ValidationResult(false, InvalidDateFormatMessage);
+
+			if (dateTime.Hour >= Rules.OpeningHour && dateTime.Hour < Rules.ClosingHour)
+				return ValidationResult.ValidResult;
+
+			return new ValidationResult(false, NonWorkingHourMessage);
+		}
+	}
+
+	public static partial class DataValidation
+	{
+		private const string InvalidNameMessage = "Неизвестный формат номера! Попробуйте еще раз";
+
+		private const string InvalidDateFormatMessage = "Неизвестный формат даты!";
+
+		private const string NonWorkingHourMessage = "В эти часы мастерская не работает!";
+
+		private static bool IsNameValid(string input) => !string.IsNullOrWhiteSpace(input);
 	}
 }
