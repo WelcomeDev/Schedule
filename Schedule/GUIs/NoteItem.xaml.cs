d3c0586ecd92f5437ba3212db401f9fb56575f1c
SharpeningInCSharp <65521226+SharpeningInCSharp@users.noreply.github.com>
2021-05-23 12:23:44 +1000

    Connection

--- Schedule/GUIs/NoteItem.xaml.cs
+++ Schedule/GUIs/NoteItem.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using Controller.DataApis;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Schedule.GUIs
 {
@@ -21,6 +11,11 @@ namespace Schedule.GUIs
 		public NoteItem()
 		{
 			InitializeComponent();
+		}
+
+		public NoteItem(INoteDisplayedData noteDisplayedData) : this()
+		{
+			DataContext = noteDisplayedData;
 		}
 	}
 }
