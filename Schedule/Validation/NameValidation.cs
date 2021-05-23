4528e818eba35aa1372b25bf208063c6f8d1e31f
SharpeningInCSharp <65521226+SharpeningInCSharp@users.noreply.github.com>
2021-05-23 16:00:44 +1000

    ValidationRules for GUI input

---
+++ Schedule/Validation/NameValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Schedule.Validation
+{
+	public class NameValidation : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
