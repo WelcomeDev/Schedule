39280b17be92d0728239a092eec60839922a04c3
SharpeningInCSharp <65521226+SharpeningInCSharp@users.noreply.github.com>
2021-05-28 18:18:30 +1000

    New abstraction - MainWindowDataContext
    It manages everything connected to NoteItem display
    It's intended to be used in MainWindow.DataContext

---
+++ Schedule/MainWindowData/MainWindowDataContext.cs
@@ -0,0 +1,64 @@
+﻿using Controller.DataApis;
+using Schedule.GUIs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Schedule
+{
+	/// <summary>
+	/// Часть, где содержится класс, управляющий данными отображения MainWindow
+	/// Создан как объект для биндинга в MainWindow.DataContext
+	/// </summary>
+	public partial class MainWindow
+	{
+		private class MainWindowData
+		{
+			private readonly List<NoteItem> allViewItems;
+
+			public ObservableCollection<NoteItem> DisplayedData { get; private set; }
+
+			public MainWindowData()
+			{
+				allViewItems = new List<NoteItem>();
+			}
+
+			public void InitSource(IEnumerable<INoteDisplayedData> result)
+			{
+				DisplayedData = new ObservableCollection<NoteItem>();
+
+				foreach (var dat in result)
+				{
+					var note = new NoteItem(dat);
+
+					allViewItems.Add(note);
+					DisplayedData.Add(note);
+				}
+			}
+
+			public void Remove(INoteDisplayedData noteDisplayedData)
+			{
+				var itemToBeRemoved = allViewItems.FirstOrDefault(x => x.NoteData.Equals(noteDisplayedData));
+
+				allViewItems.Remove(itemToBeRemoved);
+				DisplayedData.Remove(itemToBeRemoved);
+			}
+
+			public void Add(INoteDisplayedData noteDisplayedData)
+			{
+				var noteUI = new NoteItem(noteDisplayedData);
+				allViewItems.Add(noteUI);
+				DisplayedData.Add(noteUI);
+			}
+
+			public void DisplayDates(IEnumerable<INoteDisplayedData> newDisplay)
+			{
+				var res = allViewItems.Where(x => x.NoteData.Equals(newDisplay));
+
+				DisplayedData = new ObservableCollection<NoteItem>(res);
+			}
+		}
+	}
+}
